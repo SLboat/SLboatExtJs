98a7357252219a4c556a44f7943bc85f8850cd12
SLboat <slboat@me.com>
2013-11-05 18:37:09 +0800

    init

---
+++ ClipUpload.i18n.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Internationalisation file for extension extjs.
+ *  
+ * @author SLboat
+ *
+ * @licence GNU General Public Licence 2.0 or later
+ */
+ 
+$messages = array();
+ 
+/** English 
+ * @author SLboat
+ */
+$messages['en'] = array(
+		'extjs-desc' => 'Some Ext Js For SLboat Seeing',
+);
+
+
+/** Chinese Simplified 
+ * 中文简体，啊哈
+ * @mod 森亮号大船
+ */
+$messages['zh-hans'] = array(
+		'extjs-desc' = '一些额外js,css用于森亮号航海见识',     
+);
---
+++ ClipUpload.php
@@ -0,0 +1,50 @@
+<?php
+############################################################
+#Author:
+#Slboat,http://see.sl088.com
+#
+############################################################
+# Setup and Hooks for the MsUpload extension
+if (!defined('MEDIAWIKI')) {
+    echo ("This file is an extension to the MediaWiki software and cannot be used standalone.\n");
+    die(1);
+}
+
+## Register extension setup hook and credits:
+$wgExtensionCredits['parserhook'][] = array(
+    'name' => 'SLboatExJs',
+    'url' => 'http://see.sl088.com/',
+    'descriptionmsg' => 'extjs-desc',
+    'version' => '1.2',
+    'author' => '[http://see.sl088.ccom SLboat]'
+);
+
+$dir = dirname(__FILE__) . '/';
+$wgExtensionMessagesFiles['ExtJs'] = $dir . 'ExtJs.i18n.php';
+
+$wgHooks['EditPage::showEditForm:initial'][] = 'ExtJsSetup';
+
+$wgResourceModules['ext.ExtJs'] = array(
+    // load the js module
+    'scripts' => array(
+        'js/inline-attach.js',
+    ),
+    // 多语言消息文本咯
+    'messages' => array(
+        'extjs-desc',
+
+    ),
+    //本地基础路径
+    'localBasePath' => dirname(__FILE__),
+    //远程扩展路径？
+    'remoteExtPath' => 'ExtJs'
+);
+
+
+function ExtJsSetup()
+{
+    //load module
+    $wgOut->addModules('ext.ExtJs'oad');
+    
+    return true;
+}
