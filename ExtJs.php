0fbbf7828247dbb6118545617807a67b829b0f28
Sen <slboat@me.com>
2013-11-11 13:56:11 +0800

    重新梳理一些两个文件的位置,以及修理资源载入器RL打架的情况,现在看起来一切很好

--- ExtJs.php
+++ ExtJs.php
@@ -5,10 +5,11 @@
 #
 ############################################################
 # Setup and Hooks for the MsUpload extension
-if (!defined('MEDIAWIKI')) {
+if (!defined('MEDIAWIKI'))
+  {
     echo ("This file is an extension to the MediaWiki software and cannot be used standalone.\n");
     die(1);
-}
+  }
 
 ## Register extension setup hook and credits:
 $wgExtensionCredits['parserhook'][] = array(
@@ -19,7 +20,7 @@ $wgExtensionCredits['parserhook'][] = array(
     'author' => '[http://see.sl088.ccom SLboat]'
 );
 
-$dir = dirname(__FILE__) . '/';
+$dir                               = dirname(__FILE__) . '/';
 $wgExtensionMessagesFiles['ExtJs'] = $dir . 'ExtJs.i18n.php';
 
 $wgHooks['EditPage::showEditForm:initial'][] = 'ExtJs_Edit_Setup';
@@ -28,46 +29,52 @@ $wgHooks['EditPage::showEditForm:initial'][] = 'ExtJs_Edit_Setup';
 //$wgHooks['BeforePageDisplay'][] = 'ExtJs_Edit_Setup';
 
 $SLboatExtTpl = array(
-    'localBasePath' => dirname( __FILE__ ), //本地基本路径
+    'localBasePath' => dirname(__FILE__), //本地基本路径
     'remoteExtPath' => 'ExtJs', //远程基本路径...这是指向对的吗?
-    'group' => 'SLboat.ExtJs', //分组
+    'group' => 'SLboat.ExtJs' //分组
 );
 
 /* 这个钩子看起来是载入了全部扩展啥的 准备初始化的时候 */
 //$wgExtensionFunctions[] = 'ExtJs_Edit_Setup';
 
 //TODO:可以增加个extjs资源啥的
-$wgResourceModules += array( /* 引入部件索引 */    
-    'jquery.textcomplete' => $SLboatExtTpl +=  array(
+$wgResourceModules += array(
+    /* 引入部件索引 */
+    'jquery.textcomplete' => $SLboatExtTpl + array(
         //资源里的js
         'scripts' => array(
             'js/jquery.textcomplete.min.js',
         ),
         //资源里的css
         'styles' => array(
-            'css/fix_border.css', //修复边框的破碎,必须同时载入,否则可能破碎
-            'css/dropmenu.css', //下拉菜单的bootstrap缩微css            
+            'css/fixborder.css', //修复边框的破碎,必须同时载入,否则可能破碎
+            'css/dropmenu.css' //下拉菜单的bootstrap缩微css            
         ),
+        'dependencies' => array( //依赖jquery
+            'jquery'
+        )
     ), //自动完成注入完毕
-    'jquery.hotkeys' => $SLboatExtTpl +=  array(
+    'jquery.hotkeys' => $SLboatExtTpl + array(
         //资源里的js
         'scripts' => array(
-            'js/jquery.hotkeys.js',
+            'js/jquery.hotkeys.js'
         ),
-    ),
+        'dependencies' => array( //依赖jquery
+            'jquery'
+        ),
+    ), //热键完成
     /* 加入更多? */
-
 );
 
 
 function ExtJs_Edit_Setup()
-{
+  {
     //召唤全局变量
-    global $wgOut; 
-
+    global $wgOut;
+    
     //在页面输出的时候添加模块进去
     $wgOut->addModules('jquery.hotkeys');
     $wgOut->addModules('jquery.textcomplete');
     
     return true;
-}
+  }
