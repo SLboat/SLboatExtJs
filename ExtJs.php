1ba19856aa0bb422dbd34ccd72ae1f30c118356b
Sen <slboat@me.com>
2014-12-06 06:14:47 +0800

    * 修复针对V1.24

--- ExtJs.php
+++ ExtJs.php
@@ -30,7 +30,7 @@ $wgHooks['EditPage::showEditForm:initial'][] = 'ExtJs_Edit_Setup';
 
 $SLboatExtTpl = array(
     'localBasePath' => dirname(__FILE__), //本地基本路径
-    'remoteExtPath' => 'ExtJs', //远程基本路径...这是指向对的吗?
+    'remoteExtPath' => 'SLboatExtJs', //远程基本路径...这是指向对的吗?
     'group' => 'SLboat.ExtJs' //分组
 );
 
@@ -50,18 +50,14 @@ $wgResourceModules += array(
             'css/fixborder.css', //修复边框的破碎,必须同时载入,否则可能破碎
             'css/dropmenu.css' //下拉菜单的bootstrap缩微css            
         ),
-        'dependencies' => array( //依赖jquery
-            'jquery'
-        )
+
     ), //自动完成注入完毕
     'jquery.hotkeys' => $SLboatExtTpl + array(
         //资源里的js
         'scripts' => array(
             'js/jquery.hotkeys.js'
         ),
-        'dependencies' => array( //依赖jquery
-            'jquery'
-        ),
+
     ), //热键完成
     /* 加入更多? */
 );
